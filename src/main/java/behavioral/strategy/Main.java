65218a0f0c1d405168c1462985702cd992686591
litianxing <2050365570@qq.com>
2024-04-10 22:24:04 +0800

    修改策略模式代码

--- src/main/java/behavioral/strategy/Main.java
+++ src/main/java/behavioral/strategy/Main.java
@@ -1,5 +1,7 @@
 package behavioral.strategy;
 
+import java.util.ServiceLoader;
+
 /**
  * 应用场景: 支付系统 排序算法 图像处理 路由器设置 游戏开发
  */
@@ -12,7 +14,11 @@ public class Main {
         salesman.setStrategy(new StrategyB());
         salesman.show();
         // 满1000元加一元换购任意200元一下商品
-        salesman.setStrategy(new StrategyC());
-        salesman.show();
+        salesman.show(new StrategyC());
+        // 使用SPI(Service Provider Interface)机制加载所有Strategy实现类实例
+        ServiceLoader<Strategy> loader = ServiceLoader.load(Strategy.class);
+        for (Strategy strategy : loader) {
+            salesman.show(strategy);
+        }
     }
 }
--- src/main/java/behavioral/strategy/Salesman.java
+++ src/main/java/behavioral/strategy/Salesman.java
@@ -4,14 +4,26 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 /**
- * 售货员(context)
+ * 售货员(context): 上下文
  */
 @AllArgsConstructor
 @Data
 public class Salesman {
     private Strategy strategy;
 
+    /**
+     * 成员变量方式调用
+     */
     public void show() {
+        strategy.show();
+    }
+
+    /**
+     * 方法参数方式调用
+     *
+     * @param strategy strategy 策略接口
+     */
+    public void show(Strategy strategy) {
         strategy.show();
     }
 }
